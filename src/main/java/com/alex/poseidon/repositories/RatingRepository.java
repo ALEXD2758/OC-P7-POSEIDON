2f947cb10babf3d10195147b99681d3b826f7028
Alexandre Dubois <alex_2758@hotmail.com>
2020-11-24 15:33:14 +0100

    Implementation of RatingService Modification of RatingRepository

--- src/main/java/com/alex/poseidon/repositories/RatingRepository.java
+++ src/main/java/com/alex/poseidon/repositories/RatingRepository.java
@@ -1,8 +1,16 @@
 package com.alex.poseidon.repositories;
 
+import com.alex.poseidon.models.CurvePointModel;
 import com.alex.poseidon.models.RatingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RatingRepository extends JpaRepository<RatingModel, Integer> {
 
+    List<RatingModel> findAll();
+
+    List<RatingModel> findAllById(Iterable<Integer> integers);
+
+    RatingModel findById(int id);
 }
